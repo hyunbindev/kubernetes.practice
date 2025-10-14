43db6feee6fa4aa1e8b9e22cb3d3ea5af2949ed3
김현빈 <hyunbin@carrotui-MacBookAir.local>
2025-10-15 02:14:48 +0900

    k3s practice example

--- hello-app/client/src/App.tsx
+++ hello-app/client/src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './app.module.css'
 function App() {
+  const [rotate, setRotate] = useState<number>(0);
+  const addRotateDgree = ()=>{
+    setRotate(rotate+30);
+  }
   return (
     <>
-      Hello!!!
+      <h1 onClick={addRotateDgree} style={{"transform": `rotate(${rotate}deg)`}}>Hello hyunbin's k3s practice</h1>
     </>
   )
 }
-
 export default App
