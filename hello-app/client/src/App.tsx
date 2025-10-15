0dba26fba3f3a1d0edae5e0e39e1b08c91a11668
김현빈 <hyunbin@carrotui-MacBookAir.local>
2025-10-15 14:43:19 +0900

    modify css module

--- hello-app/client/src/App.tsx
+++ hello-app/client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './app.module.css'
+import './app.css'
 function App() {
   const [rotate, setRotate] = useState<number>(0);
   const addRotateDgree = ()=>{
